e42d8c422ce0fea1fa39597f04a87ff808584b8e
Gert Drapers <gert.drapers@live.com>
2022-01-25 11:51:27 -0800

    Add possessive pronouns

--- pluralize.go
+++ pluralize.go
@@ -266,6 +266,10 @@ func (c *Client) loadIrregularRules() { //nolint:funlen
 		{`has`, `have`},
 		{`this`, `these`},
 		{`that`, `those`},
+		{`my`, `our`},
+		{`its`, `their`},
+		{`his`, `their`},
+		{`her`, `their`},
 		// Words ending in with a consonant and `o`.
 		{`echo`, `echoes`},
 		{`dingo`, `dingoes`},
